ccb0d4e012f4d724f5a09e3b02b1cb6b2bc870d0
mandelshtamd <Ofwgktadgaf1984>
2021-09-22 01:46:44 +0300

    added solution of 3 and 4 task

---
+++ Lab3/PalindromeSequence/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromeSequence
+{
+    class PalindromeSequence
+    {
+        public static int ReverseInt(int num)
+        {
+            var result = 0;
+            while (num > 0)
+            {
+                result = result * 10 + num % 10;
+                num /= 10;
+            }
+            return result;
+        }
+
+        public static Tuple<int, int> PalSeq(int palindrome)
+        {
+            int seed = 0;
+            int steps = 0;
+
+            for (int i = 1; i <= palindrome; i++)
+            {
+                var currentCandidate = i;
+                while (currentCandidate < palindrome)
+                {
+                    currentCandidate += ReverseInt(currentCandidate);
+                    steps++;
+                }
+
+                if (currentCandidate == palindrome)
+                {
+                    return new Tuple<int, int>(i, steps);
+                }
+
+                steps = 0;
+            }
+            return new Tuple<int, int>(seed, steps);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var palindrome = int.Parse(Console.ReadLine());
+            var (seed, steps) = PalindromeSequence.PalSeq(palindrome);
+            Console.WriteLine($"seed = {seed}, steps = {steps}");
+        }
+    }
+}
---
+++ Lab3/Simplifier/Simplifier/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Simplifier
+{
+    internal class Simplifier
+    {
+        public static string Simplify(string arg)
+        {
+            var numerator = int.Parse(arg.Substring(0, arg.IndexOf('/')));
+            var denominator = int.Parse(arg.Substring(arg.IndexOf('/') + 1, arg.Length - arg.IndexOf('/') - 1));
+            var gcd = Gcd(numerator, denominator);
+            return (denominator / gcd) == 1 ? (numerator / gcd).ToString() : numerator / gcd + "/" + denominator / gcd;
+        }
+
+        public static int Gcd(int a, int b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                {
+                    a %= b;
+                }
+                else
+                {
+                    b %= a;
+                }
+            }
+            return a + b;
+        }
+    }
+
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var expr = Console.ReadLine();
+            var ans = Simplifier.Simplify(expr);
+            Console.WriteLine(ans);
+        }
+    }
+}
