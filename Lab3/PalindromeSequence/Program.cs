23690bd6406769120ad43ecf22f4c91c392a93b9
mandelshtamd <Ofwgktadgaf1984>
2021-09-22 14:32:18 +0300

    change input type to long

--- Lab3/PalindromeSequence/Program.cs
+++ Lab3/PalindromeSequence/Program.cs
@@ -5,9 +5,9 @@ namespace PalindromeSequence
 {
     class PalindromeSequence
     {
-        public static int ReverseInt(int num)
+        public static long ReverseLong(long num)
         {
-            var result = 0;
+            long result = 0;
             while (num > 0)
             {
                 result = result * 10 + num % 10;
@@ -16,35 +16,35 @@ namespace PalindromeSequence
             return result;
         }
 
-        public static Tuple<int, int> PalSeq(int palindrome)
+        public static Tuple<long, long> PalSeq(long palindrome)
         {
-            int seed = 0;
-            int steps = 0;
+            long seed = 0;
+            long steps = 0;
 
-            for (int i = 1; i <= palindrome; i++)
+            for (long i = 1; i <= palindrome; i++)
             {
-                var currentCandidate = i;
+                long currentCandidate = i;
                 while (currentCandidate < palindrome)
                 {
-                    currentCandidate += ReverseInt(currentCandidate);
+                    currentCandidate += ReverseLong(currentCandidate);
                     steps++;
                 }
 
                 if (currentCandidate == palindrome)
                 {
-                    return new Tuple<int, int>(i, steps);
+                    return new Tuple<long, long>(i, steps);
                 }
 
                 steps = 0;
             }
-            return new Tuple<int, int>(seed, steps);
+            return new Tuple<long, long>(seed, steps);
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            var palindrome = int.Parse(Console.ReadLine());
+            var palindrome = long.Parse(Console.ReadLine());
             var (seed, steps) = PalindromeSequence.PalSeq(palindrome);
             Console.WriteLine($"seed = {seed}, steps = {steps}");
         }
