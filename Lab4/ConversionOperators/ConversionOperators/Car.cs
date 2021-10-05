f0b1e30b9964bfc0855fd9b6141370662e43230f
mandelshtamd <Ofwgktadgaf1984>
2021-10-05 11:08:56 +0300

    added solution for interfaces task

---
+++ Lab4/ConversionOperators/ConversionOperators/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConversionOperators
+{
+    public enum Color
+    {
+        Green,
+        Red,
+        White,
+        Black
+    }
+    public class Car
+    {
+        public int MaxSpeed
+        {
+            get;
+            private set;
+        }
+
+        public Color Color
+        {
+            get;
+            private set;
+        }
+
+        public Car(int maxSpeed, Color color)
+        {
+            MaxSpeed = maxSpeed;
+            Color = color;
+        }
+    }
+}
---
+++ Lab4/ConversionOperators/ConversionOperators/Horse.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConversionOperators
+{
+    class Horse
+    {
+        public int Years
+        {
+            get;
+            private set;
+        }
+
+        public Color Color
+        {
+            get;
+            private set;
+        }
+
+        public bool IsShod
+        {
+            get;
+            private set;
+        }
+        public int MaxSpeed;
+
+        Horse(int years, bool isShod, Color color)
+        {
+            Years = years;
+            IsShod = isShod;
+            Color = color;
+        }
+
+        Horse(int years, bool isShod, Color color, int maxSpeed)
+        {
+            Years = years;
+            IsShod = isShod;
+            Color = color;
+            MaxSpeed = maxSpeed;
+        }
+
+        public static bool operator <(Horse lhs, Horse rhs)
+        {
+            return true;
+        }
+
+        public static bool operator >(Horse lhs, Horse rhs)
+        {
+            return rhs < lhs;
+        }
+
+
+        public static bool operator ==(Horse lhs, Horse rhs)
+        {
+            return lhs.IsShod == rhs.IsShod && lhs.Years == rhs.Years;
+        }
+
+        public static bool operator !=(Horse lhs, Horse rhs)
+        {
+            return lhs == rhs;
+        }
+
+        public static explicit operator Car(Horse horse)
+        {
+            return new Car(horse.MaxSpeed, horse.Color);
+        }
+    }
+}
