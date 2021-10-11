67918056ae39e8ce7aaf02167b2869e4bbf947fa
mandelshtamd <Ofwgktadgaf1984>
2021-10-11 18:41:43 +0300

    added pseudo stack solution

---
+++ Lab5/pseudo_stack/pseudo_stack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace pseudo_stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var elems = new int[]{ 1, 2, 5, 8, -9, 0 };
+            var testStack = new PseudoStack<int>(elems);
+            Console.Write($"Our stack values are: ");
+            
+            foreach(var e in elems)
+            {
+                Console.Write(e + " ");
+            }
+
+            Console.WriteLine("");
+
+
+            Console.WriteLine($"stack size = {testStack.Count}");
+            Console.Write("After pop: ");
+            for (int i = 0; i < elems.Length - 1; i++)
+            {
+                Console.Write(testStack.Pop() + ", ");
+            }
+            Console.WriteLine(testStack.Pop());
+        }
+    }
+}
---
+++ Lab5/pseudo_stack/pseudo_stack/PseudoStack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pseudo_stack
+{
+    public class PseudoStack<T>
+    {
+        private readonly List<Stack<T>> _listOfStacks = new List<Stack<T>>();
+
+        public int Count => _maxStackSize * (_listOfStacks.Count - 1) + _listOfStacks[CurrIndex].Count;
+
+        private readonly int _maxStackSize = 3;
+        private int CurrIndex => _listOfStacks.Count - 1;
+        public PseudoStack(params T[] elems)
+        {
+            foreach (var e in elems)
+            {
+                this.Push(e);
+            }
+        }
+
+        public void Push(T e)
+        {
+            if (_listOfStacks.Count == 0 || _listOfStacks[CurrIndex].Count + 1 > _maxStackSize)
+            {
+                _listOfStacks.Add(new Stack<T>());
+            }
+            _listOfStacks[CurrIndex].Push(e);
+        }
+
+        public T Pop()
+        {
+            var elemToReturn = _listOfStacks[CurrIndex].Pop();
+            if (_listOfStacks[CurrIndex].Count == 0 && CurrIndex > 0)
+            {
+                _listOfStacks.RemoveAt(CurrIndex);
+            }
+
+            return elemToReturn;
+        }
+    }
+}
