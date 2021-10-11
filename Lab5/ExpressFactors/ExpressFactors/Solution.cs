ece00d606be7779ce68e52562c6fe6220437ac34
mandelshtamd <Ofwgktadgaf1984>
2021-10-12 00:49:39 +0300

    added factorization problem

---
+++ Lab5/ExpressFactors/ExpressFactors/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressFactors
+{
+    class Solution
+    {
+        public static string ExpressFactors(int input)
+        {
+            var currentValue = input;
+            var sqrtVal = (Int32) Math.Round(Math.Sqrt(input));
+            var factorization = new Dictionary<int, int>();
+
+            var factor = 2;
+            while (currentValue > 1)
+            {
+                while (currentValue % factor == 0)
+                {
+                    currentValue /= factor;
+                    factorization.TryGetValue(factor, out var currentCount);
+                    factorization[factor] = currentCount + 1;
+                }
+
+                factor++;
+            }
+
+            var ans = new StringBuilder();
+            for (var i = 2; i <= input; i++)
+            {
+                factorization.TryGetValue(i, out var currentCount);
+
+                if (currentCount == 0) continue;
+                if (currentCount == 1) ans.Append($" x {i}");
+                if (currentCount > 1) ans.Append($" x {i}^{factorization[i]}");
+            }
+
+            return ans.ToString().Substring(3);
+        }
+    }
+}
