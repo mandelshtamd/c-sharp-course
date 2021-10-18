7ca87237bfb9bf2d98b318598a163276325720bd
mandelshtamd <Ofwgktadgaf1984>
2021-10-18 23:09:27 +0300

    add sortcollection task

---
+++ Lab6/SortCollection/SortCollection/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace SortCollection
+{
+    public class Person
+    {
+        public string Name
+        {
+            get;
+        }
+
+        public int Age
+        {
+            get;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+
+    public class PersonNameComparer : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            if (x.Name.Length == y.Name.Length) return String.Compare(x.Name.Substring(0, 1), y.Name.Substring(0, 1), StringComparison.OrdinalIgnoreCase);
+            return x.Name.Length.CompareTo(y.Name.Length);
+        }
+    }
+
+    public class PersonAgeComparer : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            return x.Age.CompareTo(y.Age);
+        }
+    }
+}
---
+++ Lab6/SortCollection/SortCollection/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personsToCompare = new List<Person>
+            {
+                new Person("George", 15),
+                new Person("Anna", 42),
+                new Person("Valeria", 8),
+                new Person("Mark", 15),
+                new Person("Anka", 32)
+            };
+
+            personsToCompare.Sort(new PersonAgeComparer());
+            Console.WriteLine("List of people sorted by age:");
+            foreach (var person in personsToCompare)
+            {
+                Console.WriteLine($"Person(Age={person.Age}, Name={person.Name})");
+            }
+
+            Console.WriteLine();
+
+            personsToCompare.Sort(new PersonNameComparer());
+            Console.WriteLine("List of people sorted by name:");
+            foreach (var person in personsToCompare)
+            {
+                Console.WriteLine($"Person(Name={person.Name}, Age={person.Age})");
+            }
+        }
+    }
+}
