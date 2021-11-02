48ac337d85d0cee3c33ebd19507294ed380eafac
mandelshtamd <Ofwgktadgaf1984>
2021-11-02 06:19:07 +0300

    add task 3

---
+++ Lab8/StringDifference/StringDifference/LevenshteinDistance.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringDifference
+{
+    public static class LevenshteinDistance
+    {
+        public static int CalculateDistance(string first, string second)
+        {
+            var firstLength = first.Length;
+            var secondLength = second.Length;
+
+            var dpDistance = new int[firstLength + 1, secondLength + 1];
+
+            if (firstLength == 0)
+                return secondLength;
+
+            if (secondLength == 0)
+                return firstLength;
+
+            for (var i = 0; i <= firstLength; dpDistance[i, 0] = i++) { }
+            for (var j = 0; j <= secondLength; dpDistance[0, j] = j++) { }
+
+            for (var i = 1; i <= firstLength; i++)
+            {
+                for (var j = 1; j <= secondLength; j++)
+                {
+                    var cost = (second[j - 1] == first[i - 1]) ? 0 : 1;
+
+                    dpDistance[i, j] = Math.Min(
+                        Math.Min(dpDistance[i - 1, j] + 1, dpDistance[i, j - 1] + 1),
+                        dpDistance[i - 1, j - 1] + cost);
+                }
+            }
+            return dpDistance[firstLength, secondLength];
+        }
+    }
+}
---
+++ Lab8/StringDifference/StringDifference/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringDifference
+{
+    // Task 3
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s1 = "Hello";
+            var s2 = "hello";
+            var dist = LevenshteinDistance.CalculateDistance(s1, s2);
+
+            Console.WriteLine(dist == 1
+                ? $"Строки {s1} и {s2} отличаются на 1 символ."
+                : $"Расстояние между строками {dist}");
+        }
+    }
+}
