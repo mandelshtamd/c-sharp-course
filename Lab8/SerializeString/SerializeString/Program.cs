c776421a8b622ba15632c318dfa4c4f82ca13368
mandelshtamd <Ofwgktadgaf1984>
2021-11-03 05:56:21 +0300

    add task 1

---
+++ Lab8/SerializeString/SerializeString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+
+namespace SerializeString
+{
+    class Program
+    {
+        static void CheckSerialization(string s, Encoding enc)
+        {
+            Byte[] encodedBytes = enc.GetBytes(s);
+            Console.WriteLine("Encoded bytes: " + BitConverter.ToString(encodedBytes));
+            string decodedString = enc.GetString(encodedBytes);
+            Console.OutputEncoding = enc;
+            Console.WriteLine("Decoded string: " + decodedString);
+            Console.WriteLine("");
+            Console.OutputEncoding = Encoding.UTF8;
+        }
+
+        static void Main()
+        {
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            // Russian
+            CheckSerialization("пример", Encoding.UTF8);
+            // Deutsch
+            CheckSerialization("Straße", Encoding.UTF8);
+            // Japaneese. You also need to change encoding in your console to see it correctly.
+            // Another option is to see it in debug mode.
+            CheckSerialization("例", Encoding.GetEncoding(932));
+        }
+    }
+}
