56631df0661071cac241652bc60a68d2b2cb5c7a
mandelshtamd <Ofwgktadgaf1984>
2021-11-19 17:43:12 +0300

    add Task1

---
+++ Lab7/Task1/Task1/Nameble.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    public interface Nameble
+    {
+        string Name { get; }
+    }
+}
---
+++ Lab7/Task1/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        public static string ConcatNames(List<Nameble> namebleList, char delimiter)
+        {
+            if (namebleList == null) 
+                throw new ArgumentNullException();
+
+            return namebleList.Skip(3).Where(x => !string.IsNullOrWhiteSpace(x.Name)).Select(x => x.Name).Aggregate((i, j) => i + delimiter + j);
+        }
+
+        static void Main(string[] args)
+        {
+            var people = new List<Nameble>() { new Person("John"), new Person("Cate"), new Person("Viktor"),
+                                        new Person("Nick"), new Person("Jane")};
+
+            var delimiter = ',';
+            var concatResult = ConcatNames(people, delimiter);
+
+            Console.WriteLine(concatResult);
+        }
+    }
+}
