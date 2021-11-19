6958ea14327500575cc5c19c6a4f3215c5b016c1
mandelshtamd <Ofwgktadgaf1984>
2021-11-19 20:18:24 +0300

    add threesum task

---
+++ Lab7/ThreeSum/ThreeSum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ThreeSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var example1 = new int[] { 0, 1, -1, -1, 2 };
+            Console.Write("For input { 0, 1, -1, -1, 2 } we get answers: ");
+            var ans = ThreeSumSolver.ThreeSum(example1);
+
+            foreach(var ar in ans)
+            {
+                Console.Write("{");
+                foreach(var el in ar) {
+                    Console.Write($"{el} ");
+                }
+                Console.Write("} ");
+            }
+        }
+    }
+}
---
+++ Lab7/ThreeSum/ThreeSum/ThreeSumSolver.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThreeSum
+{
+    class ThreeSumSolver
+    {
+        public static List<int[]> ThreeSum(int[] nums)
+        {
+            var result = new List<int[]>();
+            var len = nums.Length;
+            if (len < 3) 
+                return result;
+
+            Array.Sort(nums);
+
+            var dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < len; i++)
+            {
+                if (dict.ContainsKey(nums[i])) 
+                    dict[nums[i]] = i;
+                else 
+                    dict.Add(nums[i], i);
+            }
+
+            for (int i = 0; i < len - 2; i++)
+            {
+                if (nums[i] > 0) 
+                    break;
+
+                if (i > 0 && nums[i] == nums[i - 1]) 
+                    continue;
+
+                for (int j = i + 1; j < len - 1; j++)
+                {
+                    if (j != i + 1 && nums[j] == nums[j - 1]) 
+                        continue;
+
+                    int numsk = 0 - nums[i] - nums[j];
+                    if (dict.ContainsKey(numsk) && dict[numsk] > j)
+                        result.Add(new int[] { nums[i], nums[j], numsk });
+                }
+            }
+            return result;
+
+        }
+    }
+}
