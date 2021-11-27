97ffaae32828b24a1f5e85bd44cc9030dbd7e35b
mandelshtamd <Ofwgktadgaf1984>
2021-11-27 21:44:53 +0300

    Add task 2

---
+++ Lab9/PermutationsGenerator/PermutationsGenerator/PermutationsGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PermutationsGenerator
+{
+    public static class PermutationsGenerator
+    {
+        public static List<string> GeneratePermutations(string input)
+        {
+            var output = new List<string>();
+            if (input.Length == 1)
+            {
+                output.Add(input);
+            }
+            else
+            {
+                foreach (var c in input)
+                {
+                    var tail = input.Remove(input.IndexOf(c), 1);
+                    foreach (var tailPerms in GeneratePermutations(tail))
+                    {
+                        output.Add(c + tailPerms);
+                    }
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ Lab9/PermutationsGenerator/PermutationsGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PermutationsGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var ans = PermutationsGenerator.GeneratePermutations(input);
+            ans.Sort();
+
+            foreach(var permutation in ans)
+            {
+                Console.Write(permutation + " ");
+            }
+        }
+    }
+}
