432554012708a904aced1073172f64a4190005a2
mandelshtamd <Ofwgktadgaf1984>
2021-11-30 00:36:35 +0300

    Add task 1

---
+++ Lab13/DeadLock/DeadLock/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace DeadLock
+{
+    class Program
+    {
+        static object firstResource = new object();
+        static object secondRecourse = new object();
+
+
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(FirstThreadWorker);
+            Thread thread2 = new Thread(SecondThreadWorker);
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+        }
+
+        static void FirstThreadWorker()
+        {
+            lock (firstResource)
+            {
+                Console.WriteLine("first resource locked by first thread");
+                Thread.Sleep(500);
+                lock (secondRecourse)
+                {
+                    Console.WriteLine("Thanks for a day without deadlocks!");
+                }
+            }
+        }
+
+        static void SecondThreadWorker()
+        {
+            lock (secondRecourse)
+            {
+                Console.WriteLine("second resource locked by second thread");
+                Thread.Sleep(500);
+                lock (firstResource)
+                {
+                    Console.WriteLine("Thanks for a day without deadlocks!");
+                }
+            }
+        }
+    }
+}
