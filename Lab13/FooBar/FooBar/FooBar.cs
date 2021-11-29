0150faff45ad3eb7bf9a5a7ed9ed399a34a28d62
mandelshtamd <Ofwgktadgaf1984>
2021-11-30 02:07:18 +0300

    Add foobar task

---
+++ Lab13/FooBar/FooBar/FooBar.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FooBar
+{
+    public class FooBar
+    {
+        private readonly Semaphore foo = new Semaphore(1, 1);
+        private readonly Semaphore bar = new Semaphore(0, 1);
+
+        private int n;
+        public FooBar(int n)
+        {
+            this.n = n;
+        }
+        public void Foo(Action printFoo)
+        {
+
+            for (int i = 0; i < n; i++)
+            {
+
+                foo.WaitOne();
+
+                // printFoo() outputs "foo". Do not change or remove this line.
+                printFoo();
+
+                bar.Release();
+            }
+        }
+        public void Bar(Action printBar)
+        {
+
+            for (int i = 0; i < n; i++)
+            {
+                bar.WaitOne();
+
+                // printBar() outputs "bar". Do not change or remove this line.
+                printBar();
+
+                foo.Release();
+            }
+        }
+    }
+}
---
+++ Lab13/FooBar/FooBar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FooBar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number of foobars: ");
+            var n = int.Parse(Console.ReadLine());
+
+            var f1 = new FooBar(n);
+
+            var fooTask = Task.Factory.StartNew(() => f1.Foo(FooPrinter));
+            var barTask = Task.Factory.StartNew(() => f1.Bar(BarPrinter));
+
+            fooTask.Wait();
+            barTask.Wait();
+        }
+
+        static void FooPrinter()
+        {
+            Console.Write("foo");
+        }
+        static void BarPrinter()
+        {
+            Console.Write("bar");
+        }
+    }
+}
