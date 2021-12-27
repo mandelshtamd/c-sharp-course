4bffe37c9b922bf780562abee4e709fc99c65d6f
mandelshtamd <Ofwgktadgaf1984>
2021-12-27 22:52:52 +0300

    Add task 2

---
+++ Lab14/PrintInOrder/PrintInOrder/Foo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PrintInOrder
+{
+    public class Foo
+    {
+        private readonly Semaphore firstSem = new Semaphore(1, 1);
+        private readonly Semaphore secondSem = new Semaphore(0, 1);
+        private readonly Semaphore thirdSem = new Semaphore(0, 1);
+
+        public void first() 
+        {
+            firstSem.WaitOne();
+            Console.Write("first");
+            secondSem.Release();
+        }
+
+        public void second() 
+        {
+            secondSem.WaitOne();
+            Console.Write("second");
+            thirdSem.Release();
+        }
+
+        public void third() 
+        {
+            thirdSem.WaitOne();
+            Console.Write("third");
+            firstSem.Release();
+        }
+    }
+}
---
+++ Lab14/PrintInOrder/PrintInOrder/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace PrintInOrder
+{
+    class Program
+    {
+        static Foo testFoo = new Foo();
+
+        static void Main(string[] args)
+        {
+            Thread threadA = new Thread(testFoo.first);
+            Thread threadB = new Thread(testFoo.second);
+            Thread threadC = new Thread(testFoo.third);
+
+            var input = Console.ReadLine();
+            var parsedInput = input.Substring(1, input.Length - 2).Split(",");
+
+            foreach(var ind in parsedInput)
+            {
+                if (ind == "1")
+                {
+                    threadA.Start();
+                }
+                else if (ind == "2")
+                {
+                    threadB.Start();
+                }
+                else
+                {
+                    threadC.Start();
+                }
+            }
+
+            threadA.Join();
+            threadB.Join();
+            threadC.Join();
+        }
+    }
+}
