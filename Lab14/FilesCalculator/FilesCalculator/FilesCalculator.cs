ce891cee1a9a7e3299675537abfef9a6fcae6531
mandelshtamd <Ofwgktadgaf1984>
2021-12-28 20:43:28 +0300

    Add files calculator

---
+++ Lab14/FilesCalculator/FilesCalculator/FilesCalculator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FilesCalculator
+{
+    public static class FilesCalculator
+    {
+        public static double resultStorage = 0;
+        private static readonly Semaphore sem = new Semaphore(1, 1);
+
+        public static void HandleFiles(object objFilePaths)
+        {
+            var filePaths = (List<string>)objFilePaths;
+
+            foreach(var path in filePaths)
+            {
+                HandleFile(path);
+            }
+        }
+
+        private static void HandleFile(string filePath)
+        {
+            var (operationNum, data) = ParseFile((string)filePath);
+            var ans = Calculate(operationNum, data);
+            sem.WaitOne();
+            resultStorage += ans;
+            sem.Release();
+        }
+
+        private static Tuple<int, List<double>> ParseFile(string filePath)
+        {
+            using (FileStream fstream = new FileStream(filePath, FileMode.Open))
+            {
+                byte[] array = new byte[fstream.Length];
+                fstream.Read(array, 0, array.Length);
+                string textFromFile = System.Text.Encoding.Default.GetString(array);
+                var parsedText = textFromFile.Split("\n");
+                var operationNum = int.Parse(parsedText[0]);
+                var dataList = new List<string>(parsedText[1].Split(" ")).ConvertAll(s => double.Parse(s));
+                return new Tuple<int, List<double>>(operationNum, dataList);
+            }
+        }
+
+        private static double Calculate(int operationNum, List<double> nums)
+        {
+            if (operationNum == 1)
+            {
+                return nums.Aggregate((sum, i) => sum + i);
+            } 
+            else if (operationNum == 2)
+            {
+                return nums.Aggregate((prod, i) => prod * i);
+            } 
+            else
+            {
+                return nums.Aggregate((sum, i) => sum + i * i);
+            }
+        }
+    }
+}
---
+++ Lab14/FilesCalculator/FilesCalculator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+
+namespace FilesCalculator
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число потоков: ");
+            var threadsNum = int.Parse(Console.ReadLine());
+            var testDirectory = Path.GetFullPath(@"..\..\..\") + "testDirectory";
+
+            var files = Directory.GetFiles(testDirectory);
+
+            threadsNum = Math.Min(files.Length, threadsNum);
+            Thread[] threadsArray = new Thread[threadsNum];
+
+            var filesPerThread = (files.Length + threadsNum - 1) / threadsNum;
+
+            var threadLists = new List<List<string>>(threadsNum);
+            for (int i = 0; i < threadsNum; i++)
+            {
+                threadLists[i] = new List<string>();
+            }
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                threadLists[i / filesPerThread].Add(files[i]);
+            }
+
+            for (int i = 0; i < threadsNum; i++)
+            {
+                threadsArray[i] = new Thread(() => FilesCalculator.HandleFiles(threadLists[i]));
+                threadsArray[i].Start();
+            }
+
+            for (int i = 0; i < threadsNum; i++)
+            {
+                threadsArray[i].Join();
+            }
+            Console.WriteLine($"Получившийся результат: {FilesCalculator.resultStorage}");
+        }
+    }
+}
