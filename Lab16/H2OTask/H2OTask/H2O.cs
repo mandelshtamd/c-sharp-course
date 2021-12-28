a07e8b57710f4033290662b35344e8b6cd08b6a2
mandelshtamd <Ofwgktadgaf1984>
2021-12-28 21:45:27 +0300

    Add task3

---
+++ Lab16/H2OTask/H2OTask/H2O.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace H2OTask
+{
+    class H2O
+    {
+        object hLock = new object();
+        object oLock = new object();
+        int h = 0;
+        Barrier b = new Barrier(2);
+
+        public H2O()
+        {
+
+        }
+
+        public void Hydrogen(Action releaseHydrogen)
+        {
+            lock (hLock)
+            {
+                ++h;
+
+                releaseHydrogen();
+
+                if (h == 2)
+                {
+                    b.SignalAndWait();
+                    h = 0;
+                }
+            }
+        }
+
+        public void Oxygen(Action releaseOxygen)
+        {
+            lock (oLock)
+            {
+                releaseOxygen();
+                b.SignalAndWait();
+            }
+        }
+    }
+}
---
+++ Lab16/H2OTask/H2OTask/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace H2OTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            H2O h2o = new H2O();
+            var tasks = new List<Task>();
+            for (int i = 0; i < 3; i++)
+            {
+                var job = Task.Run(() => h2o.Oxygen(() => Console.Write("O")));
+                tasks.Add(job);
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                var job = Task.Run(() => h2o.Hydrogen(() => Console.Write("H")));
+                tasks.Add(job);
+            }
+
+            foreach(var task in tasks)
+            {
+                task.Wait();
+            }
+        }
+    }
+}
