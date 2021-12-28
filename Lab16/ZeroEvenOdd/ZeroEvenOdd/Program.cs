43a23f068242f4fb1e239f2600e91f74b109288c
mandelshtamd <Ofwgktadgaf1984>
2021-12-28 21:13:43 +0300

    Add task1

---
+++ Lab16/ZeroEvenOdd/ZeroEvenOdd/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ZeroEvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var zeroEvenOddObj = new ZeroEvenOdd(n);
+
+            var zeroTask = Task.Factory.StartNew(() => zeroEvenOddObj.Zero((i) => Console.Write($"{i}")));
+            var evenTask = Task.Factory.StartNew(() => zeroEvenOddObj.Even((i) => Console.Write($"{i}")));
+            var oddTask = Task.Factory.StartNew(() => zeroEvenOddObj.Odd((i) => Console.Write($"{i}")));
+
+
+            zeroTask.Wait();
+            evenTask.Wait();
+            oddTask.Wait();
+        }
+    }
+}
---
+++ Lab16/ZeroEvenOdd/ZeroEvenOdd/ZeroEvenOdd.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroEvenOdd
+{
+    public class ZeroEvenOdd
+    {
+        private int n;
+
+        System.Threading.Semaphore zeroSem;
+        System.Threading.Semaphore evenSem;
+        System.Threading.Semaphore oddSem;
+
+        public ZeroEvenOdd(int n)
+        {
+            this.n = n;
+
+            zeroSem = new System.Threading.Semaphore(1, 1);
+            evenSem = new System.Threading.Semaphore(0, 1);
+            oddSem = new System.Threading.Semaphore(0, 1);
+        }
+
+        public void Zero(Action<int> printNumber)
+        {
+
+            for (int i = 1; i <= this.n; i++)
+            {
+                zeroSem.WaitOne();
+                printNumber(0);
+
+                if (i % 2 == 0)
+                    evenSem.Release();
+                else
+                    oddSem.Release();
+
+            }
+        }
+
+        public void Even(Action<int> printNumber)
+        {
+            for (int i = 2; i <= this.n; i += 2)
+            {
+                evenSem.WaitOne();
+                printNumber(i);
+                zeroSem.Release();
+            }
+        }
+
+        public void Odd(Action<int> printNumber)
+        {
+            for (int i = 1; i <= this.n; i += 2)
+            {
+                oddSem.WaitOne();
+                printNumber(i);
+                zeroSem.Release();
+            }
+        }
+    }
+
+}
